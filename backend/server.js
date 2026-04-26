bc6737ce6d61a6b9862e354b524195c2dc9ec50f
ayushkumar1100 <ayushkumar4010@gmail.com>
2026-04-27 01:25:32 +0530

    ready

--- backend/server.js
+++ backend/server.js
@@ -3,25 +3,19 @@ const mongoose = require("mongoose")
 const cors = require("cors")
 const bodyParser = require("body-parser")
 const authRoutes = require("./routes/authRoutes")
-
+const connectDB = require("./db/db.js")
+const dotenv = require("dotenv");
+dotenv.config()
 const app = express()
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use("/api", authRoutes)
 
-async function connectDB() {
-    try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/digitalCard")
-        console.log("MongoDB Connected")
-    } catch (error) {
-        console.log(error)
-    }
-}
 
 connectDB()
 
 const cardRoutes = require("./routes/cardRoutes")
 app.use("/api",cardRoutes)
 
-app.listen(3000)
+app.listen(process.env.PORT || 3000)
