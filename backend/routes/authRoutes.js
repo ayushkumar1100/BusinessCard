2b21b424b95565c6b3fbae26a8c6888c06c727e6
ayushkumar1100 <ayushkumar4010@gmail.com>
2026-04-26 17:10:49 +0530

    created separate signup/login page and completed crud operations

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,10 +2,21 @@ const express = require("express")
 const router = express.Router()
 const User = require("../models/user")
 
-router.post("/register", async(req, res)=>{
-    const user = new User(req.body)
+router.post("/register", async (req, res) => {
+    const { email, password } = req.body
+
+    // 🔍 check if user exists
+    const existing = await User.findOne({ email })
+
+    if (existing) {
+        return res.json({ message: "User already exists" })
+    }
+
+    // ✅ create new user
+    const user = new User({ email, password })
     await user.save()
-    res.json(user)
+
+    res.json({ message: "User created successfully" })
 })
 
 router.post("/login", async(req, res)=>{
